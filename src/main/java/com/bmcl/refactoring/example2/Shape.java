caf7e96e9565b48f1dc0c1c895ac85cf88c01128
Pedro Venda <pedro.venda27@gmail.com>
2024-11-04 14:49:58 +0000

    Initial Commit

---
+++ src/main/java/com/bmcl/refactoring/example2/Shape.java
@@ -0,0 +1,70 @@
+package com.bmcl.refactoring.example2;
+
+
+abstract class Shape {
+    protected double x;
+    protected double y;
+
+    public Shape(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public abstract double getArea();
+    public abstract double getPerimeter();
+    public abstract void draw(GraphicFramework graphics);
+}
+
+class Rectangle extends Shape {
+    private double width;
+    private double height;
+
+    public Rectangle(double x, double y, double width, double height) {
+        super(x, y);
+        this.width = width;
+        this.height = height;
+    }
+
+    @Override
+    public double getArea() {
+        return width * height;
+    }
+
+    @Override
+    public double getPerimeter() {
+        return 2 * (width + height);
+    }
+
+    @Override
+    public void draw(GraphicFramework graphics) {
+        graphics.drawLine(x, y, x + width, y);
+        graphics.drawLine(x + width, y, x + width, y + height);
+        graphics.drawLine(x + width, y + height, x, y + height);
+        graphics.drawLine(x, y + height, x, y);
+    }
+}
+
+class Circle extends Shape {
+    private double radius;
+
+    public Circle(double x, double y, double radius) {
+        super(x, y);
+        this.radius = radius;
+    }
+
+    @Override
+    public double getArea() {
+        return Math.PI * Math.pow(radius, 2);
+    }
+
+    @Override
+    public double getPerimeter() {
+        return 2 * Math.PI * radius;
+    }
+
+    @Override
+    public void draw(GraphicFramework graphics) {
+        graphics.drawCircle(x, y, radius);
+    }
+}
+
